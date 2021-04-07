b21179702114ea14f375d90652a525936aadd471
IEatSushi <2klaus.4peter@gmail.com>
2021-04-08 00:14:31 +0200

    they didnt stage... wow

--- src/main/java/me/gorgeousone/tangledmaze/command/SettingsCommand.java
+++ src/main/java/me/gorgeousone/tangledmaze/command/SettingsCommand.java
@@ -1,2 +1,42 @@
-package me.gorgeousone.tangledmaze.command;public class SettingsCommand {
+package me.gorgeousone.tangledmaze.command;
+
+import me.gorgeousone.tangledmaze.SessionHandler;
+import me.gorgeousone.tangledmaze.cmdframework.argument.ArgType;
+import me.gorgeousone.tangledmaze.cmdframework.argument.ArgValue;
+import me.gorgeousone.tangledmaze.cmdframework.argument.Argument;
+import me.gorgeousone.tangledmaze.cmdframework.command.ArgCommand;
+import me.gorgeousone.tangledmaze.maze.MazeProperty;
+import me.gorgeousone.tangledmaze.maze.MazeSettings;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.UUID;
+
+public class SettingsCommand extends ArgCommand {
+	
+	private final SessionHandler sessionHandler;
+	
+	public SettingsCommand(SessionHandler sessionHandler) {
+		super("setting");
+		addArg(new Argument("property", ArgType.STRING, MazeProperty.commandNames()));
+		addArg(new Argument("integer", ArgType.INTEGER));
+		this.sessionHandler = sessionHandler;
+	}
+	
+	@Override
+	protected void executeArgs(CommandSender sender, ArgValue[] argValues, Set<String> usedFlags) {
+		Player player = (Player) sender;
+		UUID playerId = player.getUniqueId();
+		
+		MazeProperty property = MazeProperty.match(argValues[0].get());
+		if (property == null) {
+			sender.sendMessage("doesn't look good");
+			return;
+		}
+		int inputValue = argValues[1].getInt();
+		MazeSettings settings = sessionHandler.getSettings(playerId);
+		sender.sendMessage("Set " + property.textName() + " to " + settings.setValue(property, inputValue) + " blocks(s).");
+	}
 }
--- src/main/java/me/gorgeousone/tangledmaze/maze/MazeProperty.java
+++ src/main/java/me/gorgeousone/tangledmaze/maze/MazeProperty.java
@@ -1,2 +1,49 @@
-package me.gorgeousone.tangledmaze.maze;public class MazeProperty {
+package me.gorgeousone.tangledmaze.maze;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum MazeProperty {
+	WALL_HEIGHT(50, 2),
+	WALL_WIDTH(50, 1),
+	PATH_WIDTH(50, 1),
+	CURLINESS(10, 3),
+	ROOF_WIDTH(50, 1);
+	
+	private final int maxVal;
+	private final int defaultVal;
+	
+	MazeProperty(int maxVal, int defaultVal) {
+		this.maxVal = maxVal;
+		this.defaultVal = defaultVal;
+	}
+	
+	public int getMax() {
+		return maxVal;
+	}
+	
+	public int getDefault() {
+		return defaultVal;
+	}
+	
+	public String textName() {
+		return toString().toLowerCase().replace("_", " ");
+	}
+	
+	public String commandName() {
+		return toString().toLowerCase().replace("_", "");
+	}
+	
+	public static String[] commandNames() {
+		return Arrays.stream(values()).map(MazeProperty::commandName).toArray(String[]::new);
+	}
+	
+	public static MazeProperty match(String playerInput) {
+		for (MazeProperty dimension : MazeProperty.values()) {
+			if (dimension.commandName().equalsIgnoreCase(playerInput)) {
+				return dimension;
+			}
+		}
+		return null;
+	}
 }
