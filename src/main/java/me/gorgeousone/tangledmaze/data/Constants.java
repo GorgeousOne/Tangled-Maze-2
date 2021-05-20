834b03a0c9b8c5d26500934b2e9c8735dceed431
IEatSushi <2klaus.4peter@gmail.com>
2021-05-20 10:28:41 +0200

    WIP add material lists
    untested

--- src/main/java/me/gorgeousone/tangledmaze/data/Constants.java
+++ src/main/java/me/gorgeousone/tangledmaze/data/Constants.java
@@ -1,6 +1,11 @@
 package me.gorgeousone.tangledmaze.data;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+import java.util.TreeSet;
 
 public class Constants {
 	
@@ -13,4 +18,23 @@ public class Constants {
 			ChatColor.GREEN + "TM" +
 			ChatColor.DARK_GREEN + "] " +
 			ChatColor.YELLOW;
+	
+	public static final TreeSet<Material> TRANSPARENT_SOLIDS = new TreeSet<>();
+	public static final TreeSet<Material> TRANSPARENT_NOT_REPLACEABLES = new TreeSet<>();
+	
+	public static void loadMaterials(FileConfiguration materialLists) {
+		TRANSPARENT_SOLIDS.clear();
+		TRANSPARENT_NOT_REPLACEABLES.clear();
+		
+		for (String materialName : (List<String>) materialLists.getList("transparent-solids")) {
+			try {
+				TRANSPARENT_SOLIDS.add(Material.valueOf(materialName));
+			} catch (IllegalArgumentException ignored) {}
+		}
+		for (String materialName : (List<String>) materialLists.getList("transparent-not-replaceables")) {
+			try {
+				TRANSPARENT_NOT_REPLACEABLES.add(Material.valueOf(materialName));
+			} catch (IllegalArgumentException ignored) {}
+		}
+	}
 }
